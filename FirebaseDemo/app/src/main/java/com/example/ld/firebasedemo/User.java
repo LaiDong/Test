b150271ac79b23fa1a3f8b346911669cb9065df7
Lai Van Dong <donglv@vnpt-technology.vn>
2018-01-24 17:08:17 +0700

    Test

---
+++ FirebaseDemo/app/src/main/java/com/example/ld/firebasedemo/User.java
@@ -0,0 +1,18 @@
+package com.example.ld.firebasedemo;
+
+/**
+ * Created by ld on 24/01/2018.
+ */
+
+public class User {
+    public Double longtitude;
+    public Double latitude;
+
+    public User() {
+    }
+
+    public User(Double longtitude, Double latitude) {
+        this.longtitude = longtitude;
+        this.latitude = latitude;
+    }
+}
